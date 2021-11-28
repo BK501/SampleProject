9608bd782c3225c39e1786a83cb1c4aed3ee5c1a
BK501 <56062507+BK501@users.noreply.github.com>
2021-11-28 10:56:36 -0800

    changes done

--- zSample/src/com/app/Hello.java
+++ zSample/src/com/app/Hello.java
@@ -2,6 +2,7 @@ package com.app;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Java"+10+20);
+		System.out.println(10+20+"Java");
 	}
 }
